d0d3cf1f1ca074fa0cf1af61896b4ebec18b568a
anton-sychev <anton.sychev@footballindex.co.uk>
2017-05-25 22:08:18 +0300

    Added ChatBro isolated component

---
+++ web/app/components/Chat/ChatBro.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {PropTypes} from "react";
+
+/**
+ *  Wrapper component for chatbro iframe
+ *
+ */
+class ChatBro extends React.Component {
+
+    render() {
+
+    	let frameHeight =  this.props.height || "30%";
+
+        if (!this.props.chatId) return null;
+        return <iframe 
+	        		style={{height: frameHeight,  width: "100%"}} 
+	        		src={`https://www.chatbro.com/ru/${this.props.chatId}`}
+        		>
+        		</iframe>;
+    }
+}
+
+ChatBro.propTypes = {
+    chatId: PropTypes.string.isRequired,
+    height: PropTypes.string
+};
+
+export default ChatBro;
